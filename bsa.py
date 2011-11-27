76f750bf340a28538f3ead52024f49b67652d47c
Dan Tillberg <dan@tillberg.us>
2011-11-27 15:35:42 -0500

    Making awesome, duh.

--- bsa.py
+++ bsa.py
@@ -92,7 +92,7 @@ while file_num < file_count:
                 dataArr.append(f.read(bytes_to_read))
                 #f.read(5)
                 bytes_left -= bytes_to_read
-            data = (''.join(dataArr))
+            data = (''.join(dataArr))[header_size:]
             f2.write(data)
             parse(data[8:])
     file_num += 1
